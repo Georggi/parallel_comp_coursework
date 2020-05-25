00d9b51e23fe1be28dad3f83170f3f7bd72f751d
Georggi <bmp2558@gmail.com>
2020-05-25 16:25:10 +0300

    Implemented base structure for database class

---
+++ Database.py
@@ -0,0 +1,20 @@
+from multiprocessing import Manager, Pool
+
+
+class Database:
+    """Class that handles storage for inverted index"""
+    def __init__(self, manager: Manager):
+        """Initialize database dict with process-safe dictionary"""
+        self.__db = manager.dict()
+
+    def __getitem__(self, item: str):
+        """Implement magic for getting item from index by key
+           :item - key word for search"""
+
+        return self.__db.get(item, None)
+
+    def insert(self, item):
+        self.__db.update()
+
+    def remove(self, item):
+        return self.__db.pop(None, None)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from multiprocessing import Process
 
+var = 22
+thread_count = 1
+
 
 if __name__ == '__main__':
     pass
